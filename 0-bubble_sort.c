478ea25924e47d6b3a8ec9277926b58bc5cf192b
Winfred <winfrednginakilonzo@gmail.com>
2023-06-23 04:21:21 -0700

    fix bugs

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,35 +9,30 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
 	int temp;
-	int count = 0;
 	int swap = 0;
 
 	if (size < 2 || array == NULL)
 	{
 		return;
 	}
-	else
+	for (i = 0; i < size - 1; i++)
+	/* Iterate through the entire list*/
 	{
-		for (i = 0; i < size - 1; i++)
-		/* Iterate through the entire list*/
+		for (j = 1; j < (size - i); j++)
+		/*It gets us the next element to compare array[0] with*/
 		{
-			for (j = 1; j < (size - count); j++)
-			/*It gets us the next element to compare array[0] with*/
+			if (array[j - 1] > array[j])
 			{
-				if (array[j - 1] > array[j])
-				{
-					temp = array[j - 1];
-					array[j - 1] = array[j];
-					array[j] = temp;
-					print_array(array, size);
-					swap = 1;
-				}
+				temp = array[j - 1];
+				array[j - 1] = array[j];
+				array[j] = temp;
+				print_array(array, size);
+				swap = 1;
 			}
-			count++;
-			if (swap == 0)
-			{
-				return;
-			}
+		}
+		if (swap == 0)
+		{
+			return;
 		}
 	}
 }
